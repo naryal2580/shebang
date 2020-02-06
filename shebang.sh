6bb976dab04daa949d2cd2b8603bea356ee87342
naryal2580 <naryal2580@gmail.com>
2020-02-06 18:39:20 +0545

    some misc. betterment changes

--- shebang.sh
+++ shebang.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-main_sh=$(finger $USER|grep -oP 'Shell: \K.*')
+# main_sh=$(finger $USER|grep -oP 'Shell: \K.*')
 fullname="$PWD/$1"
 fulfname="${fullname##*/}"  # Full File Name :p
 extension="${fulfname##*.}"
@@ -8,12 +8,23 @@ filename="${fulfname%.*}"
 tmpffname="$PWD/$filename$RANDOM.$extension"
 tmpfname="${tmpffname%.*}"
 
+if type "zsh" > /dev/null; then
+	good_sh="zsh"
+elif type "bash" > /dev/null; then
+	good_sh="bash"
+else
+	good_sh="sh"
+fi
+
 rmshbng() {
 
     # $i may later be used, as it counts lines read..
 
     while IFS= read -r line; do
-        [ "$i" != "" ] && echo "$line" >> "$tmpffname"
+        if [[ $i == "" ]] && [[ $line == *shebang* ]]; then
+		line=""
+	fi
+	echo "$line" >> "$tmpffname"
         i=$((i+1))
     done < "$fullname"
 
@@ -33,27 +44,42 @@ case $extension in
 		rustc $tmpffname
 		$tmpfname
 		rm $tmpffname
+		rm $tmpfname
 	;;
 
-	sh | bash)
-		bash $filename
+	sh | bash | zsh)
+		$good_sh $filename
 	;;
 
-	zsh)
-		zsh $filename
+	go)
+		rmshbng
+		go run $tmpffname
+		rm $tmpffname
+	;;
+
+	c)
+		gcc -o $tmpfname $filename
+		$tmpfname
+	;;
+
+	js)
+		rmshbng
+		node $tmpffname
+		rm $tmpffname
 	;;
 
 	fish)
 		fish $filename
 	;;
 
-	"")
+	"")  # a JIC* case  [*Just in case]
+		# this is a fin' rare case ;)
 		$main_sh $filename
 	;;
 
 	*)
-		if [[ $filename != *.* ]]; then
-			$main_sh $filename
+		if [[ $filename != *.* ]]; then  # if no extension
+			$good_sh $filename
 		else
 			echo "$extension not supported yet..";
 		fi
